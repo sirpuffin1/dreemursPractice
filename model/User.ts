f8d867ca51eead89225ff2d90b5b47984ef738d7
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-04 16:56:28 -0800

    feat(auth): create db connection, model

---
+++ model/User.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String, required: true, unique: true
+    },
+    email: {
+        type: String, required: true, unique: true
+    },
+    hashedPassword: {
+        type: String, required: true, minlength: 5
+    }
+})
+
+const User = mongoose.models.User || mongoose.model("User", userSchema)
+export default User
