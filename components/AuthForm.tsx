5d548afbea47184c8f489611654b8ce6af0af6f6
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-06 14:01:11 -0800

    feat(auth): configure auth form to handle sign in/up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,11 +1,13 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useState, FormEvent, ChangeEvent } from "react";
 import AuthFormInput from "./AuthFormInput";
+import Router from 'next/router';
+import axios from "axios";
 
 const defaultEmailPassword = {
   email: "",
-  password: ""
-}
+  password: "",
+};
 
 const AuthForm = () => {
   const { data: session } = useSession();
@@ -16,30 +18,85 @@ const AuthForm = () => {
   };
 
   const [username, setUsername] = useState("");
-  const [ emailPassword, setEmailPassword ] = useState(defaultEmailPassword)
-  const { email, password } = emailPassword
-  
+  const [emailPassword, setEmailPassword] = useState(defaultEmailPassword);
+  const { email, password } = emailPassword;
+
+  const redirectToHome = () => {
+    const { pathname } = Router;
+    if(pathname == "/auth") Router.push("/home")
+  }
+
+  const registerUser = async () => {
+    const registerCredentials = {username, ...emailPassword}
+    console.log(registerCredentials)
+    const res = await axios.post("/api/register", {
+      username, ...emailPassword
+    }, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(async () => {
+      await loginUser();
+    }).catch((error) => console.log(error))
+
+    console.log(res)
+  };
+
+  const loginUser = async () => {
+    const res:any = signIn("credentials", {
+      ...emailPassword,
+      redirect: false,
+      callbackUrl: `${window.location.origin}`,
+    });
+    res.error ? console.log(res.error) : redirectToHome()
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    authType == "Login" ? "logging in" : "registering";
-    console.log(emailPassword)
+    authType == "Login" ? loginUser() : registerUser();
   };
 
   const handleEmailPassword = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target
-    setEmailPassword({...emailPassword, [name]: value})
-    console.log(emailPassword)
-  }
+    const { name, value } = event.target;
+    setEmailPassword({ ...emailPassword, [name]: value });
+    console.log(emailPassword);
+  };
 
   const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value)
-    console.log(username)
-  }
+    setUsername(event.target.value);
+    console.log(username);
+  };
+
   return (
     <div className="form-control">
+      <h2 className="card-title justify-center">{authType}</h2>
+      <div className="flex">
+        <p className="text-center">
+          {authType == "Login"
+            ? "Not registered yet?"
+            : "Already have an account? "}
+        </p>
+        <button
+          className="text-black"
+          onClick={() => setAuthType(oppAuthType[authType])}
+        >
+          {oppAuthType[authType]}
+        </button>
+      </div>
+
       <form onSubmit={handleSubmit}>
+        {authType != "Login" && (
+          <AuthFormInput
+            type="text"
+            placeholder="BigStepper45"
+            value={username}
+            labelText="Username"
+            onChange={handleUsername}
+          />
+        )}
+
         <AuthFormInput
           type="email"
           placeholder="me@myemail.com"
@@ -49,15 +106,7 @@ const AuthForm = () => {
           onChange={handleEmailPassword}
         />
 
-<AuthFormInput
-          type="text"
-          placeholder="BigStepper45"
-          value={username}
-          labelText="Username"
-          onChange={handleUsername}
-        />
-
-<AuthFormInput
+        <AuthFormInput
           type="password"
           placeholder="shhhhh"
           value={password}
