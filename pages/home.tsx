f65ccd896b3cde01c6d9ec877430874f3fc1df81
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-10 16:39:51 -0800

    feat(home): conditionally display username modal

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,13 +1,40 @@
-import { useSession } from "next-auth/react";
+import { GetServerSideProps, GetStaticProps } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next"
+import RegistrationModal from "../components/RegistrationModal";
 
-const home = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const  session  = await unstable_getServerSession(context.req, context.res, authOptions)
+    console.log(session, 'line 6')
+    if(session) {
+        const signedInUserId = await prisma.user.findFirst({
+            where: {
+                id: session?.user
+            }
+        })
+        return {
+            props: {username: signedInUserId.username}
+        }
+    }
+
+    return {
+        props: {},
+    }
+}
+
+const home = (props: any) => {
     const { data: session } = useSession()
+   
 
-    return (
-        <div>
-            {session?.user.id}
-        </div>
-    );
+    if(!props.username) {
+        return (
+            <>
+            {/* The button to open modal */}
+            <RegistrationModal/>
+            </>
+        )
+    }
 }
 
 export default home;
