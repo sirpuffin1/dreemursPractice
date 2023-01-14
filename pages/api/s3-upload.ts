312e023757cb31bb07c93cfbc91e3dcd96010d4d
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-13 16:21:17 -0800

    feat(api): create s3 upload endpoint

---
+++ pages/api/s3-upload.ts
@@ -0,0 +1,9 @@
+import { APIRoute } from "next-s3-upload";
+
+export default APIRoute.configure({
+    async key(req, filename) {
+        let userId= req.body.userId;
+
+        return `${userId}/${filename}`;
+    }
+})
