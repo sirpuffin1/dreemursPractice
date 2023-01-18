47b6bea87bf96faa76ab8d42675e6eb1d39c0ade
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-17 18:15:34 -0800

    ref(loader): update styling

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     const style = { "--value": 70 } as React.CSSProperties
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center">
       <div className="animate-spin radial-progress bg-primary text-primary-content border-4 border-primary " style={style}></div>
     </div>
     );
