bf018b98ecaafa803e26062f030984defcf69cd4
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-20 17:01:26 -0800

    ref(nav): update logo style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="navbar-start">
         <Link className="btn btn-ghost flex gap-2" href="/home">
           <Image src={nightlyLogo} alt="Nightly Logo" height={50} width={50} />
-          <h2 className="text-2xl font-normal">Nightly</h2>
+          <p className="text-2xl font-light capitalize">Nightly</p>
         </Link>
       </div>
       <div className="navbar-end">
