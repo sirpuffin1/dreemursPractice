f812d8c59ea8a0b2424a871a9662298a7fde0cc3
sirpuffin1 <angelcruz981@yahoo.com>
2023-01-20 16:33:16 -0800

    feat(hero): display newly designed logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center gap-5 mt-4 flex-col lg:flex-row">
+    <div className="flex justify-center items-center mt-4 flex-col relative">
       <Image
-        src="/cozy-bedroom.png"
+        src="/nightly-high-resolution-logo-white-on-transparent-background (1).png"
         alt="A very sleepy person"
         width={500}
         height={500}
       />
-      <div className="flex items-center flex-col justify-around">
-        <h1 className="text-white text-6xl">Let all your dreams come true</h1>
-        <Link href="/register">
-          <button className="btn btn-wide bg-sleepy-purple text-nightly-purple hover:bg-nightly-purple">
-            Register
-          </button>
-        </Link>
-      </div>
+      <Image
+        src="/nightly-website-favicon-color.png"
+        alt="A very sleepy person"
+        className="absolute top-6 md:top-12"
+        width={200}
+        height={200}
+       
+      />
     </div>
   );
 };
